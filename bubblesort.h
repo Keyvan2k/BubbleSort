c627141f27376eb0577381d5efd4b95aa285e5eb
Keyvan2k <69215231+Keyvan2k@users.noreply.github.com>
2021-05-11 20:26:38 +0200

    Update bubblesort.h

--- bubblesort.h
+++ bubblesort.h
@@ -1,18 +1,6 @@
-// funzione per riordinare gli elementi tramite bubble sort
+#ifndef MOSTRAMESE_H_INCLUDED
+#define MOSTRAMESE_H_INCLUDED
+
 void bubbleSort(int a[])
-{
-    //  ciclo dei numeri passati
-    for (unsigned int pass = 1; pass  < SIZE; ++pass)
-    {
-        // ciclo per confrontare i numeri ad ogni passata
-        for(size_t j = 0; j < SIZE - 1; ++j){
-            // scambia gli elementi se non sono in ordine
-            if(a[j] > a[j+ 1])
-            {
-                unsigned int mantieni = a[j];
-                a[j] = a[j + 1];
-                a[j + 1] = mantieni;
-            }
-        }
-    }
-}
+
+#endif
