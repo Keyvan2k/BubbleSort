59a782fd8b8c610d61fdc435d479fc2aff38a822
Keyvan2k <69215231+Keyvan2k@users.noreply.github.com>
2021-06-19 20:34:47 +0200

    Update bubblesort.c

--- bubblesort.c
+++ bubblesort.c
@@ -1,19 +1,20 @@
 #include "bubblesort.h"
+void swap(int *element1,*element2){
+    int hold;
+    hold = *element1;
+    *element1 = element2;
+    *element2 = hold;
+}
 
 void bubbleSort(int a[]){
-    //  ciclo dei numeri passati
-  
   int SIZE = sizeof(a)/sizeof(a[0]);  
-   
+   //  for loop for past numbers
 for (unsigned int pass = 1; pass  < SIZE; ++pass){
-        // ciclo per confrontare i numeri ad ogni passata
+        // for loop for confronting every number 
         for(size_t j = 0; j < SIZE - 1; ++j){
-            // scambia gli elementi se non sono in ordine
-            if(a[j] > a[j+ 1])
-            {
-                unsigned int hold = a[j];
-                a[j] = a[j + 1];
-                a[j + 1] = hold;
+            // swap elements 
+            if(a[j] > a[j+ 1]) {
+                swap(a[j-1],a[j+1])
             }
         }
     }
